558dfaa2df63c08144b4cc69fc54c1b5fc776048
aalbarqi <ashjanalbarqi2@gmail.com>
2020-10-18 18:54:54 +0300

    hooks

---
+++ build/precache-manifest.b2933161f8c4c6729b40ffd8d1f55071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef0c9cadef666b2daead064c88d98ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "726afdb9978603e638fd",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "82738bbaed158e761526",
+    "url": "/static/css/main.3918ab62.chunk.css"
+  },
+  {
+    "revision": "726afdb9978603e638fd",
+    "url": "/static/js/2.7f86c4da.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7f86c4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82738bbaed158e761526",
+    "url": "/static/js/main.e2295280.chunk.js"
+  },
+  {
+    "revision": "6ac6ae0be37ff8085e2a",
+    "url": "/static/js/runtime-main.9c0fc2f1.js"
+  }
+]);
---
+++ src/components/todo/form.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+function TodoForm(props) {
+
+  const [item, setItem] = useState({});
+
+  const handleInputChange = e => {
+    setItem({ ...item, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    props.handleSubmit(item);
+    setItem({});
+  };
+
+  return (
+    <>
+      <Card style={{ width: '20rem' }}>
+      <Card.Body>
+      
+        <h3>Add To Do Item</h3>
+        <form onSubmit={handleSubmit}>
+          <label>
+          <Card.Title>To Do Item</Card.Title>
+            <input
+              name="text"
+              placeholder="Add To Do List Item"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+          <Card.Title>Assigned To</Card.Title>
+            <input type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
+          </label>
+          <label>
+          <Card.Subtitle>Difficulty Rating</Card.Subtitle>
+            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange} />
+          </label>
+          
+          <Button type="submit">Add Item</Button>
+        </form>
+        </Card.Body>
+      </Card>
+    </>
+  );
+
+}
+
+
+export default TodoForm;
